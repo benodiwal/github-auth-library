3949039fd9ddb9eddebe30d0d15ca5c86793a79d
Sachin <s474996633@gmail.com>
2024-08-09 05:52:45 +0530

    credentials

---
+++ src/auth/credentials.ts
@@ -0,0 +1,22 @@
+class Credentials {
+    
+  constructor(
+    public accessToken: string,
+    public tokenType: string,
+    public scope: string
+  ) {}
+
+  toJSON(): Record<string, string> {
+    return {
+      access_token: this.accessToken,
+      token_type: this.tokenType,
+      scope: this.scope,
+    };
+  }
+
+  static fromJSON(json: Record<string, string>): Credentials {
+    return new Credentials(json.access_token, json.token_type, json.scope);
+  }
+}
+
+export default Credentials;
